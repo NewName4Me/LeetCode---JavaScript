20c8918f8eb2822d83d97ef2ed7b14c8ee064571
NewName4Me <tortarod@gmail.com>
2024-04-25 13:31:33 +0200

    porblema de numeros palindromos

---
+++ LeetCodeProblems/LC9_PalindromeNumber.js
@@ -0,0 +1,28 @@
+/*
+https://leetcode.com/problems/palindrome-number/description/
+Comprobar si un numero es o no palindromo
+
+*/
+
+/**
+ * esta funcion recibe un numero como parametro y debemos indicar si este es un palindromo es decir, se lee igual del derecho que del reves
+ * 
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let xReversed = 0;
+    let xCopy = x; //esta copia es necesaria, para comparar los valores revertidos no queremo utilizar la version que modificamos ahora de x
+
+    while (x > 0) { //el bucle se ejecuta mientras aun haya digitos que procesar de X
+        const digit = x % 10;
+        xReversed = xReversed * 10 + digit;
+        x = ~~(x / 10); //el simbolo de ~~ es lo mismo que usar un parseInt
+    }
+
+    return xReversed === xCopy;
+};
+
+console.log(isPalindrome(41));
+console.log(isPalindrome(757));
+console.log(isPalindrome(3993));

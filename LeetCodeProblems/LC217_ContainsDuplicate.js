4e0cde689b9fdd8086bac41405387dccd0a5567b
NewName4Me <tortarod@gmail.com>
2024-05-16 23:06:14 +0200

    añadir planteamiento para la resolucion de este problema

--- LeetCodeProblems/LC217_ContainsDuplicate.js
+++ LeetCodeProblems/LC217_ContainsDuplicate.js
@@ -7,6 +7,12 @@ nota importante que acabo de aprender, en un forEach un return no interrumpe su 
 por eso usamos un for of
 
 utilizamos un Set como "diccionario " dado que solo contiene valores unicos
+
+La logica de este problema es sencilla, recorremos cada valor de nuestro array y lo añadimos 
+a otra lista, en este caso un set porque almacena valores unicos pero tambien podriamos un
+array, el set solo añade seguridad de que no repitamos valores,y comprobamos, si 
+nuestro diccionario de valores ya contiene algun valor que estemos mirando nos 
+devuelve true, si al final de mirar todos no se repite ninguno devolvemos false
 */
 
 /**
